cab31796ed86807da798705b619e127a10003b3a
Inês Duarte Quelhas <I052054@corp.santander.pt>
2020-04-21 18:24:20 +0100

    First commit

---
+++ src/main/java/test/isban/cib/testjavaspringbootapi/TestJavaspringbootApiApplication.java
@@ -0,0 +1,13 @@
+package test.isban.cib.testjavaspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaspringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJavaspringbootApiApplication.class, args);
+	}
+
+}
